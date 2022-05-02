902b1b0cfcf7996f5da5e83449b1b9ddd57465b9
Arthur Vincent Simon <developarvin@gmail.com>
2022-05-02 14:16:04 +0800

    Implement Chrome extension devtools page

--- public/devtools.js
+++ public/devtools.js
@@ -1,13 +1,9 @@
-let currentTab = undefined;
-
 chrome.devtools.panels.create(
     "Chrome Extension Hello World", // title
-    '', // icon
-    "devtools.html",
+    'images/icon.png', // icon path
+    "devtools.html", // page path
     async (panel) => {
-        if (!currentTab) {
-            let [tab] = await chrome.tabs.query({ active: true });
-            currentTab = tab;
-        }
+        console.log(panel)
     }
 );
+
